32fc114d53f7ba4e460075f6bd77983b940b2c3d
Sergey Kamaev <63342992+nybc4ik@users.noreply.github.com>
2022-03-07 18:36:27 +0300

    Update Test.java

--- lab 3/Test.java
+++ lab 3/Test.java
@@ -3,6 +3,7 @@ package com.company;
 public class Test {
     int n;
     int [] myArray;
+    int [] expectedArray;
     public Test() {
         System.out.println("На ввод поступает 5 чисел:");
         n = 5;
@@ -12,6 +13,13 @@ public class Test {
         myArray[2] = 112;
         myArray[3] = 123;
         myArray[4] = 1234;
-        Numbers num = new Numbers(myArray);
+
+        expectedArray = new int[n];
+        expectedArray[0]=111;
+        expectedArray[1]=112;
+        expectedArray[2]=123;
+        expectedArray[3]=1234;
+        expectedArray[4]=12345;
+        Numbers num = new Numbers(myArray, expectedArray);
     }
 }
