8c3860a3f51b7fa3fd3278a9df16bc85964c2ef3
Sergey Kamaev <63342992+nybc4ik@users.noreply.github.com>
2022-03-14 13:18:52 +0300

    Update Main.java

--- lab 3/Main.java
+++ lab 3/Main.java
@@ -1,1 +1,22 @@
+package com.company;
+import java.util.Scanner;
 
+public class Main {
+    static int a;
+    static int [] Arr;
+    static int [] expectedArray;
+
+    public static void main(String[] args) {
+        Arr = new int [0];
+        System.out.println("Выберети метод работы программы тестрование(0) или ручной ввод(1) ");
+        Scanner console = new Scanner(System.in);
+        a = console.nextInt();
+        if (a == 0) {
+            System.out.println("Запуск тестов");
+            Test exm = new Test();}
+        else {
+            System.out.println("Введите данные");
+            Numbers num = new Numbers(Arr, expectedArray);
+        }
+    }
+}
